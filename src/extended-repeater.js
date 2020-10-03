80745230565546aa52ea3f17a7d2d50206d56302
Nick Frolov <frolovnick0@gmail.com>
2020-10-03 21:36:31 +0300

    feat: add extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let rezult = '';
+    for (let i = 1; i <= (options.repeatTimes || 1); i++) {
+      rezult +=str;
+        if (options.addition !== undefined) {
+          for (let j = 1; j <= (options.additionRepeatTimes || 1); j++) {
+             rezult += options.addition;
+               if (j !== (options.additionRepeatTimes || 1)) {
+                 rezult += (options.additionSeparator || '');
+               };
+          };
+        };
+      if (i !== (options.repeatTimes || 1)) {
+        rezult += (options.separator || '+');
+      };
+    };
+    return rezult;
+  };
+
   
