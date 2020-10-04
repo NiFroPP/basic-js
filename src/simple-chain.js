9ef0b15d05a86fb446a60b7a5aad3fae6feb7475
Nick Frolov <frolovnick0@gmail.com>
2020-10-04 17:20:59 +0300

    feat: add make chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+    chain: [],
+  
+    getLength() {
+      return this.chain.length;
+    },
+    addLink(value) {
+      this.chain.push(`${value}`);
+      return this;
+    },
+    removeLink (position) {
+      if (typeof position != "number" ||
+        position < 0 ||
+        position > this.chain.length ||
+        !Number.isInteger(position)) {
+            this.chain = [];
+            throw Error;
+            }
+      this.chain = this.chain.slice(0, position - 1).concat(this.chain.slice(position));
+      return this;
+    },
+    reverseChain() {    
+      this.chain = this.chain.reverse();
+      return this;
+    },
+    finishChain() {
+      let result = `( ${this.chain.join(' )~~( ')} )`;
+      this.chain = [];
+      return result;
+    }
+  };
 
 module.exports = chainMaker;
